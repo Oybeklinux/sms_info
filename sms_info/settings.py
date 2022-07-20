797c24acc0e7c3bc94826e37d4329d58e52d53e3
Oybeklinux <Oybeklinux@gmail.com>
2022-07-20 13:52:36 +0500

    allow all domen use API

--- sms_info/settings.py
+++ sms_info/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api.smsinfo.kannas.uz', 'www.api.smsinfo.kannas.uz', "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
