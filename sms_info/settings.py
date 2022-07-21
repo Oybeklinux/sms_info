af13697563ccc4bafa7bb2259fa33422dfb708cb
Oybeklinux <Oybeklinux@gmail.com>
2022-07-21 15:25:04 +0500

    CORS fix

--- sms_info/settings.py
+++ sms_info/settings.py
@@ -166,3 +166,21 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000'
 ]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'https://localhost:3000',
+    'https://localhost:8000',
+    'http://localhost:3000',
+    'http://localhost:8000'
+]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+        'accept',
+        'accept-encoding',
+        'authorization',
+        'content-type',
+        'dnt',
+        'origin',
+        'user-agent',
+        'x-csrftoken',
+        'x-requested-with',
+]
