1d936af15264fc3569b5519f233434596ec810af
Oybeklinux <Oybeklinux@gmail.com>
2022-07-23 14:14:22 +0500

    add many lesson API added

--- main/views.py
+++ main/views.py
@@ -31,6 +31,21 @@ class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+    def create(self, request, *args, **kwargs):
+        """
+        #checks if post request data is an array initializes serializer with many=True
+        else executes default CreateModelMixin.create function
+        """
+        is_many = isinstance(request.data, list)
+        if not is_many:
+            return super(LessonViewSet, self).create(request, *args, **kwargs)
+        else:
+            serializer = self.get_serializer(data=request.data, many=True)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class LessonStudentViewSet(viewsets.ModelViewSet):
     queryset = LessonStudent.objects.all()
